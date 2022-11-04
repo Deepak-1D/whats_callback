bf3e4905b69951e8d311f009f5a3a2ca62b2f378
Deepak <68777614+Deepak-1D@users.noreply.github.com>
2022-11-04 11:56:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ async def get_message(id:str, request: Request):
             #session.rollback()
     
         
-        requests.post("https://c7f3-103-130-204-202.in.ngrok.io/bot?verify_co=CPNrQTPdhwYTdCjGU6ub",json=recv_data )
+        requests.post("https://0df4-35-207-202-6.in.ngrok.io/bot?verify_co=CPNrQTPdhwYTdCjGU6ub",json=recv_data )
         #requests.post("https://492f-35-207-202-6.in.ngrok.io/bot?verify_co=CPNrQTPdhwYTdCjGU6ub",json=recv_data )
         
    
