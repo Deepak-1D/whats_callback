e3717cf3f3956f9d9fea242d268a233f65c4f97c
Deepak <68777614+Deepak-1D@users.noreply.github.com>
2023-02-27 18:00:22 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -8,8 +8,8 @@ app = FastAPI()
 
 
 def send_req(dict_data):
-    requests.post("http://sit.rigelsoft.com:8487/bot?verify_co=CPNrQTPdhwYTdCjGU6ub",json=dict_data )
-    #requests.post("https://1651-35-207-202-6.in.ngrok.io/bot?verify_co=CPNrQTPdhwYTdCjGU6ub",json=dict_data )
+    #requests.post("http://sit.rigelsoft.com:8487/bot?verify_co=CPNrQTPdhwYTdCjGU6ub",json=dict_data )
+    requests.post("https://34d5-35-207-202-6.in.ngrok.io/bot?verify_co=CPNrQTPdhwYTdCjGU6ub",json=dict_data )
     
 
 
